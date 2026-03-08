12aaf4e6c8f5676c47991571e0600393f84fe3f0
Ruslan <r_yankiv@penguin>
2026-03-08 15:34:13 -0500

    form fix

--- main.js
+++ main.js
@@ -198,17 +198,9 @@ function initPhoneFormatting() {
   phoneInputs.forEach(input => {
     input.addEventListener('input', (e) => {
       let value = e.target.value.replace(/\D/g, '');
-      
-      if (value.length > 0) {
-        if (value.length <= 3) {
-          value = `(${value}`;
-        } else if (value.length <= 6) {
-          value = `(${value.slice(0, 3)}) ${value.slice(3)}`;
-        } else {
-          value = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(6, 10)}`;
-        }
+      if (value.length > 10) {
+        value = value.slice(0, 10);
       }
-      
       e.target.value = value;
     });
 
